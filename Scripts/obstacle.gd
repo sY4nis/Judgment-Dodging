ecbcb2184127eb1f6ee06a504dbdcfd6f6bb453d
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2026-04-19 09:40:50 +0200

    Asteroid collisino feed back

--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -5,8 +5,20 @@ extends Area2D
 # leurs vitesses sera generé au hasard
 #il y'aura 3 types d'asteroides en terme de forme 
 # leurs tailleurs sera aussi generé au hasard
+@onready var Supernova = $SUPERNOVA
+
+
+
+func shake():
+	var tween = create_tween()
+	var original_pos = position
+	for i in 5:
+		tween.tween_property(self, "position", original_pos + Vector2(randf_range(-5, 5), randf_range(-5, 5)), 0.05)
+		tween.tween_property(self, "position", original_pos, 0.05)
 
 func _on_body_entered(body):
 	if body.is_in_group("Players"):
 		print("touché")
+		Supernova.play()
+		shake()
 	pass # Replace with function body.
