9230f1f7a93ba7c4b7a6af822e72fb07a17bb4d5
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2026-04-19 10:11:15 +0200

    Asteroid will move randomly

--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -2,10 +2,11 @@ extends Area2D
 #Les asteroids seront destructibles 
 #Au contacte du joueur ils explosent et tuent le joueur
 # il se deplaseront au hasard 
-# leurs vitesses sera generé au hasard
-#il y'aura 3 types d'asteroides en terme de forme 
+# leurs vitesses sera generé au hasard 
 # leurs tailleurs sera aussi generé au hasard
 @onready var Supernova = $SUPERNOVA
+@export var speed : int = 300
+@export var velocity = Vector2(1,1)
 
 
 
@@ -13,7 +14,7 @@ func shake():
 	var tween = create_tween()
 	var original_pos = position
 	for i in 5:
-		tween.tween_property(self, "position", original_pos + Vector2(randf_range(-5, 5), randf_range(-5, 5)), 0.05)
+		tween.tween_property(self, "position", original_pos + Vector2(randf_range(-60,60), randf_range(-60, 60)), 0.05)
 		tween.tween_property(self, "position", original_pos, 0.05)
 
 func _on_body_entered(body):
@@ -22,3 +23,9 @@ func _on_body_entered(body):
 		Supernova.play()
 		shake()
 	pass # Replace with function body.
+	
+func _process(delta):
+	position += velocity * speed * delta
+
+func _ready() -> void :
+	velocity = Vector2(randf_range(-1,1), randf_range(-1, 1)).normalized()
